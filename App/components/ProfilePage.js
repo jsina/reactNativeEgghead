5a27d248f4f60a2cbc682bb8c1157f949d623908
jsina <jsina.maleki@gmail.com>
2017-07-31 15:49:32 +0430

    WIP: get data from props

--- App/components/ProfilePage.js
+++ App/components/ProfilePage.js
@@ -5,9 +5,28 @@ import api from '../utils/api';
 export default class ProfilePage extends React.Component {
     render() {
         return (
-            <View>
+            <View style={styles.container}>
                 <Text>{this.props.info.name}</Text>
             </View>
         )
     }
 }
+
+var styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#f32',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    searchBtn: {
+        backgroundColor: 'white',
+        padding: 10,
+        marginTop: 10
+    },
+    input: {
+        margin: 10,
+        padding: 10,
+        backgroundColor: 'white'
+    }
+})
